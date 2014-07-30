c047646ebed4cd9518dc34e1a0a5541b0320248e
Anna <anna.voloshchuk@gmail.com>
2014-07-30 03:08:05 +0300

    1st

---
+++ MvcApplicationToTestSignalR/Hub/MySignalRHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MvcApplicationToTestSignalR.Hub
+{
+    public class MySignalRHub: Microsoft.AspNet.SignalR.Hub
+    {
+        public void MyMethod(int n)
+        {
+            var res = n * n;
+            Clients.All.Pow(res);
+        }
+    }
+}
